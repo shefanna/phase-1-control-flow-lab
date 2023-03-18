ad099ef059324fbfc1b4056de4d25962533f09dd
shefinannathampy <shefin04annathampy94@gmail.com>
2023-03-17 21:09:08 -0400

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if(distance<400)
+    return 'This one is on me!';
+  else if (distance>400 && distance < 2000)
+    return 'That will be twenty bucks.'
+  else if (distance>2000 && distance<2500)
+    return 'I will gladly take your thirty bucks.'
+    else 
+    return 'No can do.'
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city==='NYC'?'Ok, sounds good.':'No go.'
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let a;
+  switch(tip) {
+    case "generous":
+      a = 'Thank you so much.';
+      break;
+    case "not as generous":
+      a = 'Thank you.';
+      break; 
+    default:
+      a = 'Bye.';
+  }
+  return a;
+  
 }
